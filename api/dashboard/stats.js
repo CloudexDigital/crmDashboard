6ff7db4e14936b03636b0f5cadae5b769739fb33
Tristan Francis <tristanfrancis@192-168-66-239.local>
2025-06-13 11:13:27 +0200

    updated dbContext import

--- api/dashboard/stats.js
+++ api/dashboard/stats.js
@@ -1,5 +1,5 @@
-import { dbConnect } from '../dbConnect';
-import Client from "../../lib/models/clients";
+import { dbConnect } from '../../lib/dbConnect';
+import Client from '../../lib/models/clients';
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
